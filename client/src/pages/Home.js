bd61ede2f86f84d0a3825ad0093d50ffe8788590
Karl Brewster <mark.moehring@outlook.com>
2022-08-06 22:22:37 -0500

    round two design, cleaner look with jumbotron

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import Jumbotron from "../components/Jumbotron";
 
 const Home = () => {
   return (
     <div className="container">
+      <Jumbotron />
       <CategoryMenu />
       <ProductList />
       <Cart />
